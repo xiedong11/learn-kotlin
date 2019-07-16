0ddf4552fe329727e66707d2f6d131b318fd8548
xiedong <xiedong11@aliyun.com>
2019-07-16 09:44:14 +0800

    初始化hello kotlin

---
+++ app/src/main/java/com/zhuandian/learnkotlin/HelloKotlinActivity.kt
@@ -0,0 +1,23 @@
+package com.zhuandian.learnkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hello_kotlin.*
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.toast
+
+class HelloKotlinActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_hello_kotlin)
+
+        tv_text.text = "Kotlin ····"
+
+        btn_click.setOnClickListener { btn_click.text = "按钮被点击" }
+        btn_long_press.setOnLongClickListener { btn_long_press.text = "按钮被长按";true }
+
+        btn_click_show_toast.setOnClickListener { toast("按钮被点击了") }
+        btn_long_press_show_toast.setOnLongClickListener({ toast("按钮被长按");true })
+    }
+}
---
+++ app/src/main/java/com/zhuandian/learnkotlin/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.zhuandian.learnkotlin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        btn_hello.setOnClickListener { startActivity<HelloKotlinActivity>() }
+
+    }
+}
